89b808b13d67fa68d1e71b64c66eb65ccab81141
George Olson and Leah Nelson <george@>
2016-06-06 15:21:11 -0700

    add sentence word counter function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,38 @@
 $(function(){
   $("form#sentence").submit(function(event) {
+    // var sentence = $("input#sentence").val();
+    // var words = sentence.split(" ");
+    //
+    // function isBigEnough(word){
+    //   return word.length >= 3;
+    // }
+    // var newWords = words.filter(isBigEnough);
+    // newWords.reverse();
+    // newString = newWords.join(" ");
+    // alert(newString);
+
     var sentence = $("input#sentence").val();
     var words = sentence.split(" ");
-      var newWords = words.map(function(word){
-        if (word.length >= 3) {
-          return word
-        }
-      });
-    newWords.reverse();
-    newString = newWords.join(" ");
-    alert(newString);
 
-  event.preventDefault();
+      var occur = {};
+      for (var i = 0; i < words.length; i++) {
+          occur[words[i]] = (occur[words[i]] || 0) + 1;
+      }
+
+      var array_keys = new Array();
+      var array_values = new Array();
+
+      for (var key in occur) {
+        array_keys.push(key);
+        array_values.push(occur[key]);
+      }
+
+      array_keys.forEach(function(key, value) {
+        $("ul#wordOccur").append("<li>" + key + ": " + array_values[value] + "</li>");
+      });
+
+        console.log(array_keys);
+        console.log(array_values);
+        event.preventDefault();
   });
 });
